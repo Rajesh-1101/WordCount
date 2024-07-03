cf533dac03af6c94163bfb5c9e5e46097498a119
Rajesh-1101 <zurangerajesh@gmail.com>
2024-07-03 09:56:58 +0530

    Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,26 +2,23 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object WordCount {
   def main(args: Array[String]): Unit = {
-    // Create Spark configuration with master URL
+    
     val conf = new SparkConf()
       .setAppName("WordCount")
-      .setMaster("local[*]")  // Use "local" for running locally or specify your Spark cluster URL
+      .setMaster("local[*]")  
 
-    // Create SparkContext using the configuration
     val sc = new SparkContext(conf)
 
     try {
-      // Read input text file into an RDD
+      
       val inputPath = "C:/Users/zuran/IdeaProjects/Data/wordcount.txt"
       val textFile = sc.textFile(inputPath)
 
-      // Perform word count
       val wordCounts = textFile
-        .flatMap(line => line.trim.split("\\s+"))  // Split each line into words
-        .map(word => (word.toLowerCase, 1))        // Convert each word into a (word, 1) pair
-        .reduceByKey(_ + _)                        // Reduce by key to count occurrences of each word
+        .flatMap(line => line.trim.split("\\s+")) 
+        .map(word => (word.toLowerCase, 1))       
+        .reduceByKey(_ + _)                        
 
-      // Save the word counts to output directory
       val outputPath = "Output/wordcount_result"
       wordCounts.saveAsTextFile(outputPath)
 
@@ -29,7 +26,7 @@ object WordCount {
     } catch {
       case e: Exception => println(s"Error processing Spark job: ${e.getMessage}")
     } finally {
-      // Stop SparkContext to release resources
+     
       sc.stop()
     }
   }
